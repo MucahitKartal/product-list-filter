f8435d5d95835478c0d2f380db8752007c0c7fdd
Mucahit <mucahit.kartal2015@gmail.com>
2023-12-12 21:20:56 +0300

    forgot to commit at start, now final

---
+++ starter/src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Button, Container, Stack } from "react-bootstrap";
+import "./Header.scss";
+import { categories } from "../../helper/data";
+
+export const Header = ({ setClick }) => {
+  return (
+    <Container className="header">
+      <h1>Products List</h1>
+      <Stack
+        direction="vertical"
+        gap={3}
+        className="btns justify-content-center flex-md-row"
+      >
+        {categories.map((category) => (
+          <Button onClick={(e) => setClick(e.target.textContent)}>
+            {category.toUpperCase()}
+          </Button>
+        ))}
+      </Stack>
+    </Container>
+  );
+};
